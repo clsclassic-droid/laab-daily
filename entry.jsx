8351589d0272d17e1ef52ad97554d4554e50d563
clsclassic-droid <cls.classic@gmail.com>
2026-09-03 10:08:18 +0700

    Add files via upload

--- entry.jsx
+++ entry.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./laab-daily-entry-v15.jsx";
+import App from "./laab-daily-entry-v17.jsx";
 createRoot(document.getElementById("root")).render(React.createElement(App));
